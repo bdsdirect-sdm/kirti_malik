e8e363e9412f4d612593ced36db2011089c3194f
kirti <bdsdirect.sdm@smartdatainc.net>
2024-11-11 08:39:34 +0530

    k

---
+++ backend/src/middlewares/multer.ts
@@ -0,0 +1,24 @@
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+const uploadsDir=path.join(__dirname,'..','uploads');
+
+if(!fs.existsSync(uploadsDir)){
+    fs.mkdirSync(uploadsDir,{recursive:true});
+}
+
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,uploadsDir);
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+})
+
+const upload=multer({storage});
+
+export const uploadFile=upload.fields([
+    {name:'MedicalDocuments',maxCount:1}
+])
